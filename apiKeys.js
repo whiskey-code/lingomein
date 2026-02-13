8cf47f128c56a27192390ea2517487a6fab6b3c7
Clint Robinson <you@example.com>
2026-02-13 09:13:28 -0600

    Restore apiKeys.js for EAS build

---
+++ apiKeys.js
@@ -0,0 +1,3 @@
+// DO NOT commit this file to version control!
+export const GOOGLE_TRANSLATE_API_KEY = 'AIzaSyC9Xa6kyDBam0iGF03Ff4lkTLEyjzOpulY';
+export const GOOGLE_TTS_API_KEY = 'AIzaSyDsYnx1QxnhkuF3-DnDu_fk73sgTYvkea8';
